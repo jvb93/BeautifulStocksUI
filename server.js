3848f5bb3cc55682c3894bf0a4f58e279dbec8f5
JVB <1976577+jvb93@users.noreply.github.com>
2018-02-15 15:31:21 -0800

    prod req proxy

--- server.js
+++ server.js
@@ -3,9 +3,21 @@
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
+var request = require('request');
 
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
+
+app.use('/api', function(req, res) {
+    var uurl = 'https://sandbox.tradier.com' + req.url;
+    var options = {
+        url: uurl,
+        headers: {
+            'accept': 'application/json',
+        }
+    }
+    req.pipe(request(options)).pipe(res);
+  });
 
 var port = process.env.PORT || 5000;
 app.listen(port);
