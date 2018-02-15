9afc6c757e6fcb7b19916c1e18be60b330feddfa
JVB <1976577+jvb93@users.noreply.github.com>
2018-02-14 17:11:18 -0800

    added some heroku things

--- src/App.vue
+++ src/App.vue
@@ -13,25 +13,25 @@ export default {
 <style>
 @import url('https://fonts.googleapis.com/css?family=Oswald:200,400|Roboto:100,300');
 html, body{
-  background-color: #23272A;
-  color: white;
-  font-family: 'Roboto', sans-serif;
+  background-color: #23272A !important;
+  color: white !important;
+  font-family: 'Roboto', sans-serif !important;
 
 }
 .font-oswald{ 
-    font-family: 'Oswald', sans-serif;
+    font-family: 'Oswald', sans-serif !important;
 }
 
 .font-oswald-light{ 
-    font-family: 'Oswald', sans-serif;
-    font-weight: 200;
+    font-family: 'Oswald', sans-serif !important;
+    font-weight: 200 !important;
 }
 
 .font-roboto{
-    font-family: 'Roboto', sans-serif;
+    font-family: 'Roboto', sans-serif !important;
 }
 .list-group-item{
-  background-color: transparent;
+  background-color: transparent !important;
 }
 
 </style>
